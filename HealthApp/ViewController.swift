95c205578983db5d8ed2e7d5424f2cdb8240ca38
Luke Davis <russell.davis@okstate.edu>
2018-12-28 19:55:03 -0600

    Initial Commit

---
+++ HealthApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthApp
+//
+//  Created by App Center on 12/28/18.
+//  Copyright © 2018 rlukedavis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
